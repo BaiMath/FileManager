f4bf41b50de4735434685cfed801bdb6e6303c93
kingjack <wbqingheng@163.com>
2014-02-08 15:50:06 +0800

    fileManager
    the project commit

---
+++ FileManage/FileManage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileManage
+//
+//  Created by user on 14-2-8.
+//  Copyright (c) 2014年 Jack.King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FileManage/FileManage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FileManage
+//
+//  Created by user on 14-2-8.
+//  Copyright (c) 2014年 Jack.King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
